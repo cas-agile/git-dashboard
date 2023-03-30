e8b32dedf23438e0209034ddd1724f3de065d680
NotXia <35894453+NotXia@users.noreply.github.com>
2023-03-30 09:31:48 +0200

    Add safer clone
    Co-authored-by: GenGrim76 <gennaro.grimaudo@unipa.it>

--- backend/utilities/gitinspector.js
+++ backend/utilities/gitinspector.js
@@ -1,5 +1,6 @@
 const { getProjectById } = require("../utilities/gitlabAPI");
 const exec = require('util').promisify(require("child_process").exec);
+const spawn = require("child_process").spawn;
 const { v4: uuidv4 } = require('uuid');
 const path = require("path");
 const fs = require("fs").promises;
@@ -9,7 +10,7 @@ const GitinspectorModel = require("../models/gitinspector");
 
 /**
  * Clones a repository in a temporary directory
- * @returns {string} Path of the cloned repo
+ * @returns {Promise<string>} Path of the cloned repo
  */
 async function cloneRepository(req, repo_id, branch) {
     const repo_data = await getProjectById(req, repo_id);
@@ -17,9 +18,18 @@ async function cloneRepository(req, repo_id, branch) {
     const schema = process.env.GITLAB_URL.match(/(^\w+:|^)\/\//)[0];
     const directory_name = uuidv4();
 
-    await exec(`git clone ${schema}oauth2:${req.user.access}@${clone_url} ${directory_name} -b ${branch}`, { cwd: process.env.TMP_DIR });
-
-    return path.join(process.env.TMP_DIR, directory_name);
+    return new Promise((resolve, rejects) => {
+        const git_clone = spawn("git", [ "clone", `${schema}oauth2:${req.user.access}@${clone_url}`, directory_name, "-b", branch ], { cwd: process.env.TMP_DIR });
+        
+        git_clone.on("close", (exit_code) => {
+            if (exit_code === 0) {
+                resolve( path.join(process.env.TMP_DIR, directory_name) );
+            }
+            else {
+                rejects();
+            }
+        });
+    });
 }
 
 async function getLastCommitHash(repo_path) {
